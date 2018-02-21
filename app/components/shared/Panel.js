592b15d33e76a0bba822710a443ed2f621bc7bba
kc <you@example.com>
2018-02-21 16:10:09 +0100

    add cdn static files

--- app/components/shared/Panel.js
+++ app/components/shared/Panel.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 class Panel extends Component{
     render() {
         return(
-            <div className="panel panel-default">
-                <div className="panel-body"><Title content={this.props.title}/>
+            <div className="card card-header">
+                <div className="card-block"><Title content={this.props.title}/>
                 <p>{this.props.description}</p>
                 </div>
             </div>
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import HerosList from './app/components/hero/HerosList';
-import HeroDetails from './app/components/hero/HeroDetails';
+import 'tether/dist/css/tether.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
+import HerosList from './app/components/hero/HerosList';
+import HeroDetails from './app/components/hero/HeroDetails';
 
 ReactDOM.render(
   <Router history={browserHistory}>
