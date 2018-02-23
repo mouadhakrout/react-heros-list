c5bee93e7f651a7a19fa533557853c887fdb6e13
unknown <mouadhakrout@gmail.com>
2018-02-23 14:58:04 +0100

    add slick carousel slider for heros cards display

--- app/components/hero/HerosList.js
+++ app/components/hero/HerosList.js
@@ -2,25 +2,33 @@ import React , {Component} from 'react';
 import PropTypes from "prop-types";
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import Slider from 'react-slick';
 import * as stuffActions from '../../actions/stuffActions';
 import Title from '../shared/Title';
 import Card from '../shared/Card';
 class HerosList extends Component{
   constructor(props) {
    super(props);
-
    this.state = {heros: []};
  }
  componentDidMount() {
    this.props.stuffActions.fetchHeros();
  }
-  render() {
+ render() {
+      const settings = {
+          dots: false,
+          infinite: true,
+          autoplay: true,
+          speed: 200,
+          slidesToShow: 4,
+          slidesToScroll: 1
+      };
     const herosList = this.props.heros.map(hero => {
-      return(<div key={"hero"+ hero.id} className="col-sm-6 col-md-4 col-lg-3 mt-4"><Card heroId={hero.id} content={hero.name} imageSrc={hero.thumbnail.path} /></div>
-      )
+      return(<div key={"hero"+ hero.id} ><Card heroId={hero.id} content={hero.name} imageSrc={hero.thumbnail.path} /></div>)
     });
-    return(<div className="container"><Title color={"#ec008c"} content={"Liste des super héros :"} /><div className="row">{herosList}</div></div>
-    );
+      return(<div className="container"><Title color={"#ec008c"} content={"Liste des super héros :"} />
+        <Slider ref={c => this.slider = c} {...settings}>{herosList}</Slider>
+      </div>);
   }
 }
 HerosList.propTypes = {
